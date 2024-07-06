0909deb9ead09a0e928e7a3652dd76e36fe89806
Mkhuzo <zulumkhuzo@gmail.com>
2024-07-06 13:59:01 +0800

    fix text editor

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
-import ReactQuill from "react-quill";
+// import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 function TextEditor({ value, onChange }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // or a loading spinner
+  }
+
   //   const [value, setValue] = useState("");
 
   //   useEffect(() => {
